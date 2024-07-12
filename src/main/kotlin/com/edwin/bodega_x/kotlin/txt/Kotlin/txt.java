b21ac4174c06fa7f1a26f5adf7d9a3e394bcaacd
Edwin950821 <sanjuane74@gmail.com>
2024-07-12 12:25:40 -0500

    Añadido datos

--- src/main/kotlin/com/edwin/bodega_x/kotlin/txt/Kotlin/txt.java
+++ src/main/kotlin/com/edwin/bodega_x/kotlin/txt/Kotlin/txt.java
@@ -1,4 +1,4 @@
 package com.edwin.bodega_x.kotlin.txt.Kotlin;
 
-//Correo Electronico: sanjuane74@gmail.com
-//Contraseña: INgenieria1995
+//Correo Electronico: sanjuane74@gmail.com  //
+//Contraseña: INgenieria1995 //
